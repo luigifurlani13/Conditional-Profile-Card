8db0c5f9eb6a085af151d1963296545dc225ae86
luigifurlani13 <luis.furlan@icloud.com>
2021-06-22 06:21:41 +0000

    finished with inserts

--- public/bundle.js
+++ public/bundle.js
@@ -699,8 +699,27 @@ __webpack_require__.r(__webpack_exports__);
 /**
  *  EDIT ONLY INSIDE THIS RENDER FUNCTION
  *  This function is called every time the user changes types or changes any input
+ * 
+    {
+        includeCover: true, // if includeCover is true the algorithm should
+        background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da", // this is the url of the image that will used as background for the profile cover
+        avatarURL: "https://randomuser.me/api/portraits/women/42.jpg", // this is the url for the profile avatar
+        socialMediaPosition: "left", // social media bar position (left or right)
+        
+        twitter: null, // social media usernames
+        github: "alesanchezr",
+        linkedin: null,
+        instagram: null,
+
+        name: null,
+        lastname: null,
+        role: null,
+        country: null,
+        city: null
+    }
  */
 function render(variables = {}) {
+  console.log("These are the current variables: ", variables); //print on the console
   // here we ask the logical questions to make decisions on how to build the html
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
@@ -710,14 +729,14 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>${variables.name || 'Empty name'}</h1>
-          <h2>${variables.role || 'Empty Role'}</h2>
-          <h3>${variables.city || ''}, ${variables.country || ''}</h3>
+          <h1>${variables.name} ${variables.lastname}</h1>
+          <h2>${variables.role}</h2>
+          <h3>${variables.city}, ${variables.country}</h3>
           <ul class="${variables.socialMediaPosition}">
-            ${variables.twitter && `<li><a href="https://twitter.com/alesanchezr"><i class="fa fa-twitter"></i></a></li>`}
-            ${variables.github && `<li><a href="https://github.com/alesanchezr"><i class="fa fa-github"></i></a></li>`}
-            ${variables.linkedin && `<li><a href="https://linkedin.com/alesanchezr"><i class="fa fa-linkedin"></i></a></li>`}
-            ${variables.instagram && `<li><a href="https://instagram.com/alesanchezr"><i class="fa fa-instagram"></i></a></li>`}
+            <li><a href="https://twitter.com/${variables.twitter}"><i class="fa fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${variables.github}"><i class="fa fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/luigifurlani"><i class="fa fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${variables.instagram}"><i class="fa fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
@@ -735,25 +754,27 @@ window.onload = function () {
     // this is the url for the profile avatar
     avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
     // social media bar position (left or right)
-    socialMediaPosition: "left",
+    socialMediaPosition: "position-right",
     // social media usernames
     twitter: null,
-    github: "alesanchezr",
+    github: null,
     linkedin: null,
     instagram: null,
-    name: null,
-    lastname: null,
-    role: null,
-    country: null,
-    city: null
+    name: "First Name",
+    lastname: "Last Name",
+    role: "Role",
+    country: "Country",
+    city: "City"
   };
-  render(window.variables);
+  render(window.variables); //render the card for the first time
+
   document.querySelectorAll(".picker").forEach(function (elm) {
     elm.addEventListener("change", function (e) {
-      const attribute = e.target.getAttribute("for");
+      // <- add a listener to every input
+      const attribute = e.target.getAttribute("for"); // when any input changes, collect the value
       let values = {};
-      values[attribute] = this.value == "" ? null : this.value == "true" ? true : this.value == "false" ? false : this.value;
-      render(Object.assign(window.variables, values));
+      values[attribute] = this.value == "" || this.value == "null" ? null : this.value == "true" ? true : this.value == "false" ? false : this.value;
+      render(Object.assign(window.variables, values)); // render again the card with new valus
     });
   });
 };
--- src/js/index.js
+++ src/js/index.js
@@ -36,11 +36,11 @@ function render(variables = {}) {
           <h1>${variables.name} ${variables.lastname}</h1>
           <h2>${variables.role}</h2>
           <h3>${variables.city}, ${variables.country}</h3>
-          <ul class="${variables.socialMediaPosition}">
-            <li><a href="https://twitter.com/${variables.twitter}"><i class="fa fa-twitter"></i></a></li>
-            <li><a href="https://github.com/${variables.github}"><i class="fa fa-github"></i></a></li>
+          <ul class="position-right">
+            <li><a href="https://twitter.com/luigifurlani"><i class="fa fa-twitter"></i></a></li>
+            <li><a href="https://github.com/luigifurlani13"><i class="fa fa-github"></i></a></li>
             <li><a href="https://linkedin.com/luigifurlani"><i class="fa fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/${variables.instagram}"><i class="fa fa-instagram"></i></a></li>
+            <li><a href="https://instagram.com/luigifurlani"><i class="fa fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
@@ -58,12 +58,12 @@ window.onload = function() {
     // this is the url for the profile avatar
     avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
     // social media bar position (left or right)
-    socialMediaPosition: "position-left",
+    socialMediaPosition: "position-right",
     // social media usernames
     twitter: null,
-    github: "luigifurlani",
+    github: null,
     linkedin: null,
-    instagram: "luigifurlani",
+    instagram: null,
     name: "First Name",
     lastname: "Last Name",
     role: "Role",
